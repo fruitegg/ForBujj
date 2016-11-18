ac7ff1a3366cc16937fc80700656f45abebcd01b
fruitegg <matthewsfox33@hotmail.com>
2016-11-18 13:20:30 -0800

    Add files via upload

---
+++ Method.py
@@ -0,0 +1,21 @@
+class A:
+
+    def __init__( self, p1 ):
+        """ the constructor """
+        self.p1 = p1
+
+    def __repr__( self ):
+        """ creates an appropriate string """
+        s = self.p1
+        return s
+
+class B: 
+
+    def __init__( self, p2 ):
+        """ the constructor """
+        p2 = self.p2
+
+    def __repr__( self ):
+        """ creates an appropriate string """
+        s = self.p2
+        return s 
